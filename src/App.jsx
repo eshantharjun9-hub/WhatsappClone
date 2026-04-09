ee5e2b196bb0f58e6b314f960dca92af5b9d058d
Eshanth <eshantharjun9@gmail.com>
2026-04-09 11:03:48 +0530

    added initial files

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
+import Chat from './Components/Chat'
+import Home from './Components/Home'
+import Login from './Components/Login'
+import NotFound from './Components/NotFound'
+import { Routes, Route } from 'react-router-dom'
 
 const App = () => {
   return (
     <>
-      
+      <h1>WhatsApp Clone</h1>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/chat/:chatid" element={<Chat />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
     </>
   )
 }
